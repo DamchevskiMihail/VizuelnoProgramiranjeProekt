b86162f14d1f2d11f7ecc142a89bead2eb36747d
DamchevskiMihail <damchevskimihail@gmail.com>
2021-08-29 19:14:00 +0200

    First commit

---
+++ Synth1/Form1.Designer.cs
@@ -0,0 +1,379 @@
+﻿
+namespace Synth1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnC3 = new System.Windows.Forms.Button();
+            this.btnD3 = new System.Windows.Forms.Button();
+            this.btnE3 = new System.Windows.Forms.Button();
+            this.btnF3 = new System.Windows.Forms.Button();
+            this.btnG3 = new System.Windows.Forms.Button();
+            this.btnA3 = new System.Windows.Forms.Button();
+            this.btnB3 = new System.Windows.Forms.Button();
+            this.btnC4 = new System.Windows.Forms.Button();
+            this.nudSustain = new System.Windows.Forms.NumericUpDown();
+            this.btnCs3 = new System.Windows.Forms.Button();
+            this.btnDs3 = new System.Windows.Forms.Button();
+            this.btnFs3 = new System.Windows.Forms.Button();
+            this.btnGs3 = new System.Windows.Forms.Button();
+            this.btnAs3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nudAmplifier = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudPitch = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSustain)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmplifier)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPitch)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnC3
+            // 
+            this.btnC3.BackColor = System.Drawing.Color.White;
+            this.btnC3.Location = new System.Drawing.Point(42, 210);
+            this.btnC3.Name = "btnC3";
+            this.btnC3.Size = new System.Drawing.Size(52, 228);
+            this.btnC3.TabIndex = 0;
+            this.btnC3.UseVisualStyleBackColor = false;
+            this.btnC3.Click += new System.EventHandler(this.btnC3_Click);
+            // 
+            // btnD3
+            // 
+            this.btnD3.BackColor = System.Drawing.Color.White;
+            this.btnD3.Location = new System.Drawing.Point(100, 210);
+            this.btnD3.Name = "btnD3";
+            this.btnD3.Size = new System.Drawing.Size(52, 228);
+            this.btnD3.TabIndex = 1;
+            this.btnD3.UseVisualStyleBackColor = false;
+            this.btnD3.Click += new System.EventHandler(this.btnD3_Click);
+            // 
+            // btnE3
+            // 
+            this.btnE3.BackColor = System.Drawing.Color.White;
+            this.btnE3.Location = new System.Drawing.Point(158, 210);
+            this.btnE3.Name = "btnE3";
+            this.btnE3.Size = new System.Drawing.Size(52, 228);
+            this.btnE3.TabIndex = 2;
+            this.btnE3.UseVisualStyleBackColor = false;
+            this.btnE3.Click += new System.EventHandler(this.btnE3_Click);
+            // 
+            // btnF3
+            // 
+            this.btnF3.BackColor = System.Drawing.Color.White;
+            this.btnF3.Location = new System.Drawing.Point(216, 210);
+            this.btnF3.Name = "btnF3";
+            this.btnF3.Size = new System.Drawing.Size(52, 228);
+            this.btnF3.TabIndex = 3;
+            this.btnF3.UseVisualStyleBackColor = false;
+            this.btnF3.Click += new System.EventHandler(this.btnF3_Click);
+            // 
+            // btnG3
+            // 
+            this.btnG3.BackColor = System.Drawing.Color.White;
+            this.btnG3.Location = new System.Drawing.Point(274, 210);
+            this.btnG3.Name = "btnG3";
+            this.btnG3.Size = new System.Drawing.Size(52, 228);
+            this.btnG3.TabIndex = 4;
+            this.btnG3.UseVisualStyleBackColor = false;
+            this.btnG3.Click += new System.EventHandler(this.btnG3_Click);
+            // 
+            // btnA3
+            // 
+            this.btnA3.BackColor = System.Drawing.Color.White;
+            this.btnA3.Location = new System.Drawing.Point(332, 210);
+            this.btnA3.Name = "btnA3";
+            this.btnA3.Size = new System.Drawing.Size(52, 228);
+            this.btnA3.TabIndex = 5;
+            this.btnA3.UseVisualStyleBackColor = false;
+            this.btnA3.Click += new System.EventHandler(this.btnA3_Click);
+            // 
+            // btnB3
+            // 
+            this.btnB3.BackColor = System.Drawing.Color.White;
+            this.btnB3.Location = new System.Drawing.Point(390, 210);
+            this.btnB3.Name = "btnB3";
+            this.btnB3.Size = new System.Drawing.Size(52, 228);
+            this.btnB3.TabIndex = 6;
+            this.btnB3.UseVisualStyleBackColor = false;
+            this.btnB3.Click += new System.EventHandler(this.btnB3_Click);
+            // 
+            // btnC4
+            // 
+            this.btnC4.BackColor = System.Drawing.Color.White;
+            this.btnC4.Location = new System.Drawing.Point(448, 210);
+            this.btnC4.Name = "btnC4";
+            this.btnC4.Size = new System.Drawing.Size(52, 228);
+            this.btnC4.TabIndex = 7;
+            this.btnC4.UseVisualStyleBackColor = false;
+            this.btnC4.Click += new System.EventHandler(this.btnC4_Click);
+            // 
+            // nudSustain
+            // 
+            this.nudSustain.BackColor = System.Drawing.SystemColors.Control;
+            this.nudSustain.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nudSustain.CausesValidation = false;
+            this.nudSustain.Font = new System.Drawing.Font("MingLiU-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudSustain.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.nudSustain.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.nudSustain.Location = new System.Drawing.Point(225, 32);
+            this.nudSustain.Maximum = new decimal(new int[] {
+            5000,
+            0,
+            0,
+            0});
+            this.nudSustain.Minimum = new decimal(new int[] {
+            250,
+            0,
+            0,
+            0});
+            this.nudSustain.Name = "nudSustain";
+            this.nudSustain.Size = new System.Drawing.Size(88, 29);
+            this.nudSustain.TabIndex = 9;
+            this.nudSustain.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudSustain.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.nudSustain.ValueChanged += new System.EventHandler(this.nudSustain_ValueChanged);
+            // 
+            // btnCs3
+            // 
+            this.btnCs3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(25)))), ((int)(((byte)(5)))));
+            this.btnCs3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnCs3.Location = new System.Drawing.Point(77, 210);
+            this.btnCs3.Name = "btnCs3";
+            this.btnCs3.Size = new System.Drawing.Size(52, 139);
+            this.btnCs3.TabIndex = 10;
+            this.btnCs3.UseVisualStyleBackColor = false;
+            this.btnCs3.Click += new System.EventHandler(this.btnCs3_Click);
+            // 
+            // btnDs3
+            // 
+            this.btnDs3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(25)))), ((int)(((byte)(5)))));
+            this.btnDs3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnDs3.Location = new System.Drawing.Point(135, 210);
+            this.btnDs3.Name = "btnDs3";
+            this.btnDs3.Size = new System.Drawing.Size(52, 139);
+            this.btnDs3.TabIndex = 11;
+            this.btnDs3.UseVisualStyleBackColor = false;
+            this.btnDs3.Click += new System.EventHandler(this.btnDs3_Click);
+            // 
+            // btnFs3
+            // 
+            this.btnFs3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(25)))), ((int)(((byte)(5)))));
+            this.btnFs3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnFs3.Location = new System.Drawing.Point(253, 210);
+            this.btnFs3.Name = "btnFs3";
+            this.btnFs3.Size = new System.Drawing.Size(52, 139);
+            this.btnFs3.TabIndex = 12;
+            this.btnFs3.UseVisualStyleBackColor = false;
+            this.btnFs3.Click += new System.EventHandler(this.btnFs3_Click);
+            // 
+            // btnGs3
+            // 
+            this.btnGs3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(25)))), ((int)(((byte)(5)))));
+            this.btnGs3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnGs3.Location = new System.Drawing.Point(311, 210);
+            this.btnGs3.Name = "btnGs3";
+            this.btnGs3.Size = new System.Drawing.Size(52, 139);
+            this.btnGs3.TabIndex = 13;
+            this.btnGs3.UseVisualStyleBackColor = false;
+            this.btnGs3.Click += new System.EventHandler(this.btnGs3_Click);
+            // 
+            // btnAs3
+            // 
+            this.btnAs3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(25)))), ((int)(((byte)(5)))));
+            this.btnAs3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnAs3.Location = new System.Drawing.Point(369, 210);
+            this.btnAs3.Name = "btnAs3";
+            this.btnAs3.Size = new System.Drawing.Size(52, 139);
+            this.btnAs3.TabIndex = 14;
+            this.btnAs3.UseVisualStyleBackColor = false;
+            this.btnAs3.Click += new System.EventHandler(this.btnAs3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(227, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 18);
+            this.label1.TabIndex = 16;
+            this.label1.Text = "Sustain";
+            // 
+            // nudAmplifier
+            // 
+            this.nudAmplifier.BackColor = System.Drawing.SystemColors.Control;
+            this.nudAmplifier.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nudAmplifier.CausesValidation = false;
+            this.nudAmplifier.Font = new System.Drawing.Font("MingLiU-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudAmplifier.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.nudAmplifier.Location = new System.Drawing.Point(448, 32);
+            this.nudAmplifier.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudAmplifier.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudAmplifier.Name = "nudAmplifier";
+            this.nudAmplifier.Size = new System.Drawing.Size(53, 29);
+            this.nudAmplifier.TabIndex = 18;
+            this.nudAmplifier.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudAmplifier.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudAmplifier.ValueChanged += new System.EventHandler(this.nudAmplifier_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(445, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 18);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Output";
+            // 
+            // nudPitch
+            // 
+            this.nudPitch.BackColor = System.Drawing.SystemColors.Control;
+            this.nudPitch.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.nudPitch.CausesValidation = false;
+            this.nudPitch.Font = new System.Drawing.Font("MingLiU-ExtB", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudPitch.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.nudPitch.Location = new System.Drawing.Point(34, 32);
+            this.nudPitch.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudPitch.Minimum = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            this.nudPitch.Name = "nudPitch";
+            this.nudPitch.Size = new System.Drawing.Size(53, 29);
+            this.nudPitch.TabIndex = 21;
+            this.nudPitch.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudPitch.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label2.Location = new System.Drawing.Point(30, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 18);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Pitch ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Synth1.Properties.Resources.istockphoto_1258849375_170667a;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(546, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nudPitch);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudAmplifier);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnAs3);
+            this.Controls.Add(this.btnGs3);
+            this.Controls.Add(this.btnFs3);
+            this.Controls.Add(this.btnDs3);
+            this.Controls.Add(this.btnCs3);
+            this.Controls.Add(this.nudSustain);
+            this.Controls.Add(this.btnC4);
+            this.Controls.Add(this.btnB3);
+            this.Controls.Add(this.btnA3);
+            this.Controls.Add(this.btnG3);
+            this.Controls.Add(this.btnF3);
+            this.Controls.Add(this.btnE3);
+            this.Controls.Add(this.btnD3);
+            this.Controls.Add(this.btnC3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.nudSustain)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudAmplifier)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPitch)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnC3;
+        private System.Windows.Forms.Button btnD3;
+        private System.Windows.Forms.Button btnE3;
+        private System.Windows.Forms.Button btnF3;
+        private System.Windows.Forms.Button btnG3;
+        private System.Windows.Forms.Button btnA3;
+        private System.Windows.Forms.Button btnB3;
+        private System.Windows.Forms.Button btnC4;
+        private System.Windows.Forms.NumericUpDown nudSustain;
+        private System.Windows.Forms.Button btnCs3;
+        private System.Windows.Forms.Button btnDs3;
+        private System.Windows.Forms.Button btnFs3;
+        private System.Windows.Forms.Button btnGs3;
+        private System.Windows.Forms.Button btnAs3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown nudAmplifier;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudPitch;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Synth1/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Wave;
+using System.Timers;
+using System.Windows;
+using Xamarin.Forms;
+
+namespace Synth1
+{
+    public partial class Form1 : Form
+    {
+
+        private double amp = 0.01;
+        private int sustain = 500;
+        private DirectSoundOut output = null;
+        private BlockAlignReductionStream stream = null;
+        WaveTone tone;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void GenerateSound(double freq, double amp)
+        {
+            if (output != null)
+            {
+                output.Stop();
+            }
+
+            if(nudAmplifier.Value == 2)
+            {
+                amp += 0.02;
+            }
+            else if(nudAmplifier.Value == 3)
+            {
+                amp += 0.1;
+            }
+            else
+            {
+                amp = 0.001;
+            }
+
+            if (nudPitch.Value == 2)
+            {
+               tone = new WaveTone(freq/2, amp);
+            }
+            else if(nudPitch.Value == 4)
+            {
+               tone = new WaveTone(freq*2, amp);
+            }
+            else if(nudPitch.Value == 5)
+            {
+               tone = new WaveTone(freq * 4, amp);
+            }else
+            {
+                tone = new WaveTone(freq, amp);
+            }
+
+            stream = new BlockAlignReductionStream(tone);
+            output = new DirectSoundOut();
+            output.Init(stream);
+            output.Play();
+
+            Task.Delay(new TimeSpan(0, 0, 0, 0, sustain)).ContinueWith(o => { output.Stop(); });
+            tone.Dispose();
+        }
+
+
+
+
+
+
+        private void btnC3_Click(object sender, EventArgs e)
+        {
+            GenerateSound(130.8128, amp);
+        }
+        private void btnCs3_Click(object sender, EventArgs e)
+        {
+            GenerateSound(138.5913, amp);
+        }
+        private void btnD3_Click(object sender, EventArgs e)
+        {
+            GenerateSound(146.8324, amp);
+        }
+        private void btnDs3_Click(object sender, EventArgs e)
+        {
+            GenerateSound(155.5635, amp);
+        }
+        private void btnE3_Click(object sender, EventArgs e)
+        {
+            GenerateSound(164.8138, amp);
+        }
+        private void btnF3_Click(object sender, EventArgs e)
+        {
+            GenerateSound(174.6141, amp);
+        }
+        private void btnFs3_Click(object sender, EventArgs e)
+        {
+            GenerateSound(184.9972, amp);
+        }
+        private void btnG3_Click(object sender, EventArgs e)
+        {
+            GenerateSound(195.9977, amp);
+        }
+        private void btnGs3_Click(object sender, EventArgs e)
+        {
+            GenerateSound(207.6523, amp);
+        }
+        private void btnA3_Click(object sender, EventArgs e)
+        {
+            GenerateSound(220.0, amp);
+        }
+        private void btnAs3_Click(object sender, EventArgs e)
+        {
+            GenerateSound(233.0819, amp);
+        }
+        private void btnB3_Click(object sender, EventArgs e)
+        {
+            GenerateSound(246.9417, amp);
+        }
+        private void btnC4_Click(object sender, EventArgs e)
+        {
+            GenerateSound(261.6256, amp);
+
+        }
+
+        private void nudSustain_ValueChanged(object sender, EventArgs e)
+        {
+            this.sustain = ((int)nudSustain.Value);
+        }
+
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (output != null)
+            {
+                output.Dispose();
+                output = null;
+            }
+            if (stream != null)
+            {
+                stream.Dispose();
+                stream = null;
+            }
+        }
+
+        private void nudAmplifier_ValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+
+    public class WaveTone : WaveStream
+    {
+        private double frequency;
+        private double amplitude;
+        private double time;
+        private int sampleRate = 44100;
+        private int bitRate = 16;
+        private int channel = 2;
+
+        public WaveTone(double f, double a)
+        {
+            this.time = 0;
+            this.frequency = f;
+            this.amplitude = a;
+        }
+        ~WaveTone() { }
+        public override long Position
+        {
+            get;
+            set;
+        }
+
+
+        // Brojot na bajti od audioto vo stream-ot
+        public override long Length
+        {
+            get { return long.MaxValue; }
+        }
+
+        public override WaveFormat WaveFormat
+        {
+            get { return new WaveFormat(sampleRate, bitRate, channel); }
+        }
+
+
+
+        //count e brojot na bajti sto go bara NAudio, baferot se polni so podatoci za da svirat
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            int samples = count / 2;
+            for (int i = 0; i < samples; i++)
+            {
+                
+                double sine = amplitude * Math.Sin(Math.PI * 2 * frequency * time);
+                time += 1.0 / 44100;
+
+                short truncated = (short)Math.Round(sine * (Math.Pow(2, 15) - 1));
+                buffer[i * 2] = (byte)(truncated & 0x00ff);
+                buffer[i * 2 + 1] = (byte)((truncated & 0xff00) >> 8);
+            }
+
+            return count;
+        }
+
+
+    }
+}
